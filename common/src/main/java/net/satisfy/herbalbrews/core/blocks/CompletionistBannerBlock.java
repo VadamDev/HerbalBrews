517b4eea31c56cc3b4a4d862a01a27fccd49af60
wdog5 <l1531245281@yandex.com>
2025-07-18 01:20:16 +0800

    Fix block

--- common/src/main/java/net/satisfy/herbalbrews/core/blocks/CompletionistBannerBlock.java
+++ common/src/main/java/net/satisfy/herbalbrews/core/blocks/CompletionistBannerBlock.java
@@ -1,12 +1,15 @@
 package net.satisfy.herbalbrews.core.blocks;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -40,6 +43,13 @@ public class CompletionistBannerBlock extends BaseEntityBlock {
     public CompletionistBannerBlock(Properties properties) {
         super(properties);
         makeDefaultState();
+    }
+
+    public static final MapCodec<CompletionistBannerBlock> CODEC = simpleCodec(CompletionistBannerBlock::new);
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return CODEC;
     }
 
     @Nullable
@@ -116,7 +126,7 @@ public class CompletionistBannerBlock extends BaseEntityBlock {
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, net.minecraft.world.item.TooltipFlag flag) {
+    public void appendHoverText(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag tooltipFlag) {
         if (PlatformHelper.shouldShowTooltip()) {
             tooltip.add(Component.translatable("tooltip.herbalbrews.banner.thankyou_1").withStyle(style -> style.withColor(TextColor.fromRgb(0x9ACD32))));
             tooltip.add(Component.empty());
